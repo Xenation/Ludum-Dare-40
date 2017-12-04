70a6f8aa5abe956bad648869e5a0e5c5df259302
Xenation <xenations.111@gmail.com>
2017-12-05 00:03:29 +0100

    Menu Manager and fixes

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace LD40 {
+	public class MenuManager : Singleton<MenuManager> {
+
+		public string level1;
+
+		public void Play() {
+			SceneManager.LoadScene(level1);
+		}
+
+	}
+}
