1fe306ea0a2c38b358e168237c4f4b4a4cc0b0f8
Xenation <xenations.111@gmail.com>
2017-12-20 03:01:05 +0100

    final tweaks on dungeon

--- Assets/Scripts/Editor/TimeScaler.cs
+++ Assets/Scripts/Editor/TimeScaler.cs
@@ -5,6 +5,7 @@ namespace LD40 {
 	public class TimeScaler : EditorWindow {
 
 		public float timeScale = 1f;
+		public string fileName = "";
 
 		[MenuItem("Window/Time Scaler")]
 		public static void Init() {
@@ -14,6 +15,17 @@ namespace LD40 {
 		public void OnGUI() {
 			GUILayout.Label("Time Scale:");
 			timeScale = EditorGUILayout.Slider(timeScale, 0.001f, 4f);
+			fileName = EditorGUILayout.TextField(fileName);
+			if (GUILayout.Button("SaveScreen")) {
+				string fName = fileName + ".png";
+				ScreenCapture.CaptureScreenshot(fName);
+				EditorUtility.RevealInFinder(Application.dataPath);
+			}
+			if (GUILayout.Button("16K Screenshot")) {
+				string fName = fileName + ".png";
+				ScreenCapture.CaptureScreenshot(fName, 2);
+				EditorUtility.RevealInFinder(Application.dataPath);
+			}
 		}
 
 		public void Update() {
